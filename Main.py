45b10e5fa4de7cd37b555895f4e7e778838584fa
Vivek Lalwani <viveklalwani8@gmail.com>
2025-06-04 17:06:23 +0530

    added pickle file and updated main.py

--- Main.py
+++ Main.py
@@ -5,10 +5,20 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return ("Hello World welcome to Vivek's Flask Application!")
+    return "Hello World welcome to Vivek's Flask Application!"
 
 @app.route("/get_square", methods=['POST'])
 def get_square():
+    data = request.get_json()
+    SepalLengthCm = data.get("sl")
+    SepalWidthCm = data.get("sw")
+    PetalLengthCm = data.get("pl")
+    PetalWidthCm = data.get("pw")
+
+    return jsonify()
+
+@app.route("/predict", methods=['POST'])
+def predict():
     data = request.get_json()
     number = data.get("number")
     return jsonify({"number": number ** 2})
