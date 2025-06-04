6772eef789860573278c4779e5aabe1c074420d8
Vivek Lalwani <viveklalwani8@gmail.com>
2025-06-04 16:46:05 +0530

    Changes done

--- Main.py
+++ Main.py
@@ -1,14 +1,18 @@
 from flask import Flask, request, jsonify
+
 app = Flask(__name__)
+
+
 @app.route('/', methods=['GET'])
 def home():
     return ("Hello World welcome to Vivek's Flask Application!")
 
-@app.route('/get_square', methods=['POST'])
+@app.route("/get_square", methods=['POST'])
 def get_square():
     data = request.get_json()
-    number = data.get('number')
-    return jsonify({'number': number**2})
+    number = data.get("number")
+    return jsonify({"number": number ** 2})
+
 
 if __name__ == '__main__':
     app.run()
