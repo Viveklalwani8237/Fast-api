186589761d18cd0c2dd41815d53eb4509c905dc0
Vivek Lalwani <viveklalwani8@gmail.com>
2025-06-04 15:52:09 +0530

    added post method

--- Main.py
+++ Main.py
@@ -3,5 +3,12 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def home():
     return ("Hello World welcome to Vivek's Flask Application!")
+
+@app.route('/get_square', methods=['POST'])
+def get_square():
+    data = request.get_json()
+    number = data.get('number')
+    return jsonify({'number': number**2})
+
 if __name__ == '__main__':
     app.run()
